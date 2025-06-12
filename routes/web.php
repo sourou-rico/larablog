811377dbb2ed3702bf3fb9ede6c4488cac095594
Sourou Ulrich KINHOEGBE <Ulrich.KINHOEGBE@chem.loc>
2025-06-12 17:55:04 +0100

    savepoint

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +16,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/articles/create', [UserController::class, 'create'])->name('articles.create');
+    Route::post('/articles/store', [UserController::class, 'store'])->name('articles.store');
 });
+
 
 require __DIR__.'/auth.php';
